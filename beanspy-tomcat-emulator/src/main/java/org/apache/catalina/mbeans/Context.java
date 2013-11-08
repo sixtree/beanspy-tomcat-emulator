2e3bfd2315350e88b23597d450ec7da048f4a6ac
Yamen <yamen@sixtree.com.au>
2013-11-08 15:56:23 +1100

    Added 'objectName' property to ensure correct response to MaxDepth=1 queries

--- beanspy-tomcat-emulator/src/main/java/org/apache/catalina/mbeans/Context.java
+++ beanspy-tomcat-emulator/src/main/java/org/apache/catalina/mbeans/Context.java
@@ -1,12 +1,41 @@
 package org.apache.catalina.mbeans;
 
+import javax.management.MBeanRegistration;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
-public class Context implements ContextMBean {
+public class Context implements ContextMBean, MBeanRegistration {
 	private static Logger logger = LoggerFactory.getLogger(ContextMBean.class);
+	private ObjectName objectName;
 	
 	public Context() {
 		logger.info("------> created ContextMBean");
+	}
+	
+	public String getbaseName() {
+		return "tomcat-emulator";
+	}
+	
+	public String getobjectName() {
+		return objectName.toString();
+	}
+
+	public ObjectName preRegister(MBeanServer server, ObjectName name)
+			throws Exception {
+		
+		this.objectName = name;
+		return name;
+	}
+
+	public void postRegister(Boolean registrationDone) {
+	}
+
+	public void preDeregister() throws Exception {		
+	}
+
+	public void postDeregister() {
 	}
 }
--- beanspy-tomcat-emulator/src/main/java/org/apache/catalina/mbeans/ContextMBean.java
+++ beanspy-tomcat-emulator/src/main/java/org/apache/catalina/mbeans/ContextMBean.java
@@ -1,4 +1,6 @@
 package org.apache.catalina.mbeans;
 
 public interface ContextMBean {
+	public String getbaseName();
+	public String getobjectName();
 }
